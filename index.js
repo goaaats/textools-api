e353f4eb069037e03fe0cef0c76bd983b0ade2c6
goaaats <fakecake@icloud.com>
2018-12-16 21:26:22 +0100

    Add POST route for GetOffset

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const IndexDatabase = require('./src/IndexDatabase');
 var db = new IndexDatabase(process.env.INDEX_DATA_PATH);
 var app = express();
 
+app.use(express.json());
+
 app.listen(process.env.EXPRESS_PORT, () => {
   console.log("API server running");
 });
@@ -36,4 +38,27 @@ app.get("/GetOffset/:datcat", (req, res, next) => {
       return res.send(JSON.stringify({ dataVersion: version, result: { path: req.query.path, offset: fileOffset } }));
     });
   });
+});
+
+app.post("/GetOffset/:datcat", (req, res, next) => {
+  var version = req.query.version;
+  if (!version)
+    version = db.currentVersion;
+
+  var offsets = {};
+
+  req.body.forEach(element => {
+    db.GetCat(req.params.datcat, version, function (error, category) {
+      if (error) return next(error);
+
+      category.index1.GetOffsetPath(element, function (error, fileOffset) {
+        if (error) return next(error);
+  
+        offsets[element] = fileOffset;
+      });
+    });
+  });
+
+  res.setHeader('Content-Type', 'application/json');
+  return res.send(JSON.stringify({ dataVersion: version, result: offsets }));
 });
